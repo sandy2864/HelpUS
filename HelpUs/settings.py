4f9179e97d9e7c412d7f0ec4e2609d7727a35d7f
Sandeep kumar <61756388+sandy2864@users.noreply.github.com>
2021-05-25 09:35:17 +0530

    Update settings.py

--- HelpUs/settings.py
+++ HelpUs/settings.py
@@ -38,13 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'helpapp',
-     'crispy_forms',
+    'crispy_forms',
     
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
