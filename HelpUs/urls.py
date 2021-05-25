02be565ce36d74af3cc60abe508462836ceedd21
Sandeep kumar <61756388+sandy2864@users.noreply.github.com>
2021-05-25 09:31:31 +0530

    Update urls.py

--- HelpUs/urls.py
+++ HelpUs/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('',home),
     path('upload/',upload),
     # path("",views.upload),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+    
