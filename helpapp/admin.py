a90b74175db291691739124353534b47415cf39a
Sandeep kumar <61756388+sandy2864@users.noreply.github.com>
2021-05-25 09:40:20 +0530

    Update admin.py

--- helpapp/admin.py
+++ helpapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Location,Image
-admin.site.register(Location)
+# from .models import Location,Image
+# admin.site.register(Location)
 
-admin.site.register(Image)
+# admin.site.register(Image)
 
 # Register your models here.
